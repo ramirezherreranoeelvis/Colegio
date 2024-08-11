182e348bcadc83d5354c788291e10f923d2483f9
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2024-08-11 11:52:47 -0500

    dinamismo a la tabla de historial para que se llene segun datos de un arreglo ya predefinido, solo falta tratar datos del backend base de datos

--- ColegioFrontend/src/app/components/dashboard/workspace/home/historial/historial.component.ts
+++ ColegioFrontend/src/app/components/dashboard/workspace/home/historial/historial.component.ts
@@ -1,12 +1,144 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-historial',
-  standalone: true,
-  imports: [],
-  templateUrl: './historial.component.html',
-  styleUrl: './historial.component.scss'
+	selector: 'app-historial',
+	standalone: true,
+	imports: [],
+	templateUrl: './historial.component.html',
+	styleUrl: './historial.component.scss'
 })
 export class HistorialComponent {
-
+	ndias = 5
+	historial = [
+		{
+			name: "Semana Actual",
+			dias: [
+				{
+					name: "lunes",
+					entrada: {
+						content: "03/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "03/06/2024",
+						status: "asistio"
+					},
+					elementoFinal: false
+				},
+				{
+					name: "martes",
+					entrada: {
+						content: "04/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "04/06/2024",
+						status: ""
+					},
+					elementoFinal: false
+				},
+				{
+					name: "miercoles",
+					entrada: {
+						content: "05/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "05/06/2024",
+						status: ""
+					},
+					elementoFinal: false
+				},
+				{
+					name: "jueves",
+					entrada: {
+						content: "06/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "06/06/2024",
+						status: ""
+					},
+					elementoFinal: false
+				},
+				{
+					name: "viernes",
+					entrada: {
+						content: "07/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "07/06/2024",
+						status: ""
+					},
+					elementoFinal: true
+				}
+			]
+		},
+		{
+			name: "Semana Pasada",
+			dias: [
+				{
+					name: "lunes",
+					entrada: {
+						content: "03/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "03/06/2024",
+						status: "falto"
+					},
+					elementoFinal: false
+				},
+				{
+					name: "martes",
+					entrada: {
+						content: "04/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "04/06/2024",
+						status: ""
+					},
+					elementoFinal: false
+				},
+				{
+					name: "miercoles",
+					entrada: {
+						content: "05/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "05/06/2024",
+						status: ""
+					},
+					elementoFinal: false
+				},
+				{
+					name: "jueves",
+					entrada: {
+						content: "06/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "06/06/2024",
+						status: ""
+					},
+					elementoFinal: false
+				},
+				{
+					name: "viernes",
+					entrada: {
+						content: "07/06/2024",
+						status: ""
+					},
+					salida: {
+						content: "07/06/2024",
+						status: ""
+					},
+					elementoFinal: true
+				}
+			]
+		}
+	]
 }
