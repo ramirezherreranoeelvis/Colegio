abba4fdaa8184a748706a7fafb856c40aee9c9d4
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2024-08-19 00:02:04 -0500

    creacion de repositorios listos para las nuevs entidades :)

---
+++ ColegioBackend/src/main/java/com/exam/colegio/repository/course/content/IGeneralContentRepository.java
@@ -0,0 +1,13 @@
+package com.exam.colegio.repository.course.content;
+
+import com.exam.colegio.model.course.content.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Gatomontes
+ */
+@Repository
+public interface ISessionRepository extends JpaRepository<Session, Integer> {
+
+}
