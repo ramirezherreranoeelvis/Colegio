d61e16796b3d927e54d51317452318c92b9ced08
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2024-08-25 19:33:56 -0500

    dieño para notas generales

---
+++ ColegioBackend/src/main/java/com/exam/colegio/controller/workSpace/NotasController.java
@@ -0,0 +1,9 @@
+package com.exam.colegio.controller.workspace;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+@RestController
+public class Enrollment {
+}
