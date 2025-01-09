cbd5a77d649d870560c6647ecec0b16d53b6dd56
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-01-09 06:05:01 -0500

    procedure

--- databbase/procedures/buscarEstudintesPorCurso.sql
+++ databbase/procedures/buscarEstudintesPorCurso.sql
@@ -1,5 +1,9 @@
-select 
-        tc.*
-from person p
-join teacherCourseScheduled tc on p.idPerson = tc.idTeacher
-where dni = '99995978'
+-- mostrar alumnos de un curso en especifico
+SELECT 
+        *
+FROM content cnt
+JOIN coursescheduled c ON cnt.idCourseScheduled = c.idCourseScheduled
+JOIN enrollment e ON c.idEnrollment = e.idEnrollment
+JOIN enrollmentstudent es ON e.idEnrollment = es.idEnrollment
+JOIN person p ON es.idStudent = p.idPerson
+WHERE cnt.idContent = 1 AND cnt.typeContent = 'session'
--- databbase/procedures/buscarEstudintesPorCurso2.sql
+++
@@ -1,5 +0,0 @@
-select 
-        tc.salary
-from person p
-join teacherCourseScheduled tc on p.idPerson = tc.idTeacher
-where dni = '99995978'
