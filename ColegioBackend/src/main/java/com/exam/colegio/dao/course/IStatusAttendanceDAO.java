dec0ea8ac4eb9b72c5b422ba602aa608127361d0
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-06-21 20:15:04 -0500

    delete: database and database

--- ColegioBackend/src/main/java/com/exam/colegio/dao/course/IStatusAttendanceDAO.java
+++
@@ -1,9 +0,0 @@
-package com.exam.colegio.dao.course;
-
-import java.util.Optional;
-
-import com.exam.colegio.model.course.StatusAttendance;
-
-public interface IStatusAttendanceDAO {
-        Optional<StatusAttendance> findByID(Integer id);
-}
--- ColegioBackend/src/main/java/com/exam/colegio/dao/person/ITeacherDAO.java
+++
@@ -1,7 +0,0 @@
-package com.exam.colegio.dao.person;
-
-import org.springframework.http.ResponseEntity;
-
-public interface ITeacherDAO {
-        ResponseEntity<?> findCoursesByYear(String year, String dni);
-}
